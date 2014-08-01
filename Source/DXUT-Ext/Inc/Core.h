079ec304e54c5e1a7caec8987393b40c88c7ad33
Aeron Zhou <aeronzhou@qq.com>
2014-08-01 19:28:54 +0800

    提交几个文件

---
+++ Source/DXUT-Ext/Inc/Core.h
@@ -0,0 +1,7 @@
+#ifndef _CORE_H_
+#define _CORE_H_
+
+// м╥нд╪Ч
+#include "SmartPtr.h"
+
+#endif
---
+++ Source/DXUT-Ext/Inc/SmartPtr.h
@@ -0,0 +1,19 @@
+#ifndef _SMART_PTR_H_
+#define _SMART_PTR_H_
+
+#include <memory>
+#include <functional>
+
+
+// 智能指针
+#define TSharedPtr std::shared_ptr
+
+// COM对象的智能指针
+// 可以在释放的时候调用Release
+template <typename T>
+inline TSharedPtr<T> MakeCOMPtr(T* Ptr)
+{
+	return Ptr ? TSharedPtr<T>(Ptr, std::mem_fn(&T::Release)) : TSharedPtr<T>();
+}
+
+#endif
